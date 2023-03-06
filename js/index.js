b2450e57fad59bfe4ac5a88fbe5cd3947da120a4
brygmos <daniel-best@yandex.ru>
2023-03-07 01:04:56 +0500

    m

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 function toggleCardBtns() {
     // console.log('over')
 }
+
+function burgerHandler() {
+    let bars = document.querySelector('.burger__bars').children;
+    bars[1].classList.toggle('decreaseWidth');
+    bars[0].classList.toggle('rotateFirst');
+    bars[2].classList.toggle('rotateThird');
+    document.querySelector('.navigation').classList.toggle('hidden')
+}
