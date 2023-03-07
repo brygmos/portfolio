f2c8d258001f94ba40d8f2b329c1e9b523f081ab
brygmos <daniel-best@yandex.ru>
2023-03-07 15:31:51 +0500

    burger test

--- js/index.js
+++ js/index.js
@@ -2,17 +2,36 @@ let header = document.querySelector(".header");
 let headerHeight = window.getComputedStyle(header).getPropertyValue('height');
 let prevScrollPos = window.pageYOffset;
 
-function burgerHandler() {
+function usingMobile() {
+    const mediaQuery = window.matchMedia('(max-width: 1000px)')
+    return mediaQuery.matches;
+
+}
+
+function morphBurger() {
     let bars = document.querySelector('.burger__bars').children;
     bars[1].classList.toggle('decreaseWidth');
     bars[0].classList.toggle('rotateFirst');
     bars[2].classList.toggle('rotateThird');
-    document.querySelector('.navigation').classList.toggle('hidden')
+}
+
+function burgerHandler() {
+    morphBurger()
+    header.classList.toggle('hidden')
 }
 
 function initHidingHeader() {
-    window.onscroll = hideNavBarOnScroll;
+    if (usingMobile()) {
+        console.log('mobile')
+        header.classList.toggle('hidden')
+
+    }
+    else {
+        window.onscroll = hideNavBarOnScroll;
+    }
 }
+
+
 
 function hideNavBarOnScroll() {
     let currentScrollPos = window.pageYOffset;
